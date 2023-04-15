3ec2f22a1b70bbe004dffdbd7df7ffefc21d9766
prince mishra <pkmak7447@gmail.com>
2023-04-15 22:07:01 +0530

    react commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[userInfo,setUserInfo] = useState({name : "",email : "",password : "", confirmPassword : ""});
+
+  function ValidateForm(){
+      let result=document.getElementById('subForm');
+      if(userInfo.name && userInfo.email && userInfo.password && userInfo.confirmPassword){
+          if(!userInfo.email.includes('@')){
+              result.innerHTML="Enter vaild Email Id"
+              result.classList.add('red')
+              return;
+          }
+          if(userInfo.password!=userInfo.confirmPassword){
+              result.innerHTML="password doesn't match "
+              result.classList.add('red')
+              return;
+          }
+          result.innerHTML="Successfully Signed Up!!!"
+          result.classList.remove('red')
+          result.classList.add('green')
+      }
+      else{
+          result.innerHTML="error>>> All the fields are mandatory"
+          result.classList.add('red')
+      }
+  }
+
+return (
+  <div className='component'>
+      <h1>Signup</h1>
+      <input placeholder='Full Name' required
+      onChange= {(e)=>setUserInfo({...userInfo , name :e.target.value })}/>
+
+      <input type='email' placeholder='Email' required
+      onChange= {(e)=>setUserInfo({...userInfo , email :e.target.value })}/>
+
+      <input placeholder='Password' required
+      onChange= {(e)=>setUserInfo({...userInfo , password :e.target.value })}/>
+
+      <input type='password' placeholder='Confirm Password' required
+      onChange= {(e)=>setUserInfo({...userInfo , confirmPassword :e.target.value })}/>
+
+      <p id='subForm'></p>
+      <button onClick={ValidateForm}>Signup</button>
+  </div>
   );
 }
 
 export default App;
+
+
